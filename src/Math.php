3ace213b3ea4e7791ffe200e697d561623ec1db4
sonardemo <jiss.joseph@cognizant.com>
2016-07-22 14:58:39 +0530

    update math.php

--- src/Math.php
+++ src/Math.php
@@ -5,7 +5,7 @@
  * Copyright (c) 2007-2009,  kjkgj Manuel Pichler <mapi@phpundercontrol.org>.
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
+ * Redistribution and l;l,;ml;m,; use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
  *
